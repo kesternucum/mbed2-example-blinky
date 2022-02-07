f4a39f9e0f2c6fc66333daeb49efe161422a7968
Kester Nucum <knucum42@students.tntech.edu>
2022-02-07 06:19:43 +0000

    Set LED0 to blink every second

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
 #include "SevenSegmentLED.h"
 #include "TextLCD.h"
 
-
 int main()
 {
     while(1)
     {
-        // LED0 blinks on and off every 1/2 second
+        // LED0 blinks on and off every 1 second
         led0 = 1;
-        wait(0.5);
+        wait(1);
         led0 = 0;
-        wait(0.5);
+        wait(1);
     }
 }
